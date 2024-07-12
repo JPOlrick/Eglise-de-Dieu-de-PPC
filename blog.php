147e968b063e3cb222d20259e2a140dea6b8ea9e
Olrick Jean-Pierre <olrick.jeanpierre02@gmail.com>
2024-07-12 14:47:24 -0400

    JPO

---
+++ blog.php
@@ -0,0 +1,374 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Demo Church | Porto - Multipurpose Website Template</title>	
+
+		<meta name="keywords" content="WebSite Template" />
+		<meta name="description" content="Porto - Multipurpose Website Template">
+		<meta name="author" content="okler.net">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+		<!-- Web Fonts  -->
+		<link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CPlayfair+Display:400,400i,700%7CSintony:400,700,800&display=swap" rel="stylesheet" type="text/css">
+
+		<!-- Vendor CSS -->
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
+		<link rel="stylesheet" href="vendor/animate/animate.compat.css">
+		<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">
+		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
+		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
+		<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="css/theme.css">
+		<link rel="stylesheet" href="css/theme-elements.css">
+		<link rel="stylesheet" href="css/theme-blog.css">
+		<link rel="stylesheet" href="css/theme-shop.css">
+
+		<!-- Demo CSS -->
+		<link rel="stylesheet" href="css/demos/demo-church.css">
+
+		<!-- Skin CSS -->
+		<link id="skinCSS" rel="stylesheet" href="css/skins/skin-church.css">
+
+		<!-- Theme Custom CSS -->
+		<link rel="stylesheet" href="css/custom.css">
+
+	</head>
+	<body>
+
+		<div class="body">
+			<?php
+			$current_page = 'blog.php';
+			 include('header.php');?>
+			<div role="main" class="main">
+
+				<section class="page-header page-header-modern bg-color-secondary page-header-lg mb-0">
+					<div class="container">
+						<div class="row my-4">
+							<div class="col-md-12 align-self-center text-center">
+								<h1 class="text-9 text-color-light custom-secondary-font font-weight-bold my-4">Blog</h1>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="section section-no-border bg-color-light m-0">
+					<div class="container">
+						<div class="row justify-content-center">
+							<div class="col-lg-4 mb-4">
+								<article class="custom-post-blog">
+									<span class="thumb-info custom-thumb-info-2">
+										<span class="thumb-info-wrapper">
+											<a href="demo-church-blog-detail.html">
+												<img src="img/demos/church/blog/blog-1.jpg" alt class="img-fluid" />
+											</a>
+										</span>
+										<span class="thumb-info-caption custom-box-shadow">
+											<span class="thumb-info-caption-text">
+												<h4 class="font-weight-bold mb-4">
+													<a href="demo-church-blog-detail.html" class="text-decoration-none custom-secondary-font text-color-dark">
+														Vestibulum tristique ipsum sed neque
+													</a>
+												</h4>
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at arcu sit amet.</p>
+											</span>
+											<span class="custom-thumb-info-post-infos text-center">
+												<ul>
+													<li class="text-uppercase">
+														<i class="icon-calendar icons"></i>
+														Oct 18
+													</li>
+													<li class="text-uppercase">
+														<i class="icon-user icons"></i>
+														John Doe
+													</li>
+												</ul>
+											</span>
+										</span>
+									</span>
+								</article>
+							</div>
+							<div class="col-lg-4 mb-4">
+								<article class="custom-post-blog">
+									<span class="thumb-info custom-thumb-info-2">
+										<span class="thumb-info-wrapper">
+											<a href="demo-church-blog-detail.html">
+												<img src="img/demos/church/blog/blog-2.jpg" alt class="img-fluid" />
+											</a>
+										</span>
+										<span class="thumb-info-caption custom-box-shadow">
+											<span class="thumb-info-caption-text">
+												<h4 class="font-weight-bold mb-4">
+													<a href="demo-church-blog-detail.html" class="text-decoration-none custom-secondary-font text-color-dark">
+														Mauris egestas dui ut dolor bibendum
+													</a>
+												</h4>
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at arcu sit amet.</p>
+											</span>
+											<span class="custom-thumb-info-post-infos text-center">
+												<ul>
+													<li class="text-uppercase">
+														<i class="icon-calendar icons"></i>
+														Oct 20
+													</li>
+													<li class="text-uppercase">
+														<i class="icon-user icons"></i>
+														John Doe
+													</li>
+												</ul>
+											</span>
+										</span>
+									</span>
+								</article>
+							</div>
+							<div class="col-lg-4 mb-4">
+								<article class="custom-post-blog">
+									<span class="thumb-info custom-thumb-info-2">
+										<span class="thumb-info-wrapper">
+											<a href="demo-church-blog-detail.html">
+												<img src="img/demos/church/blog/blog-3.jpg" alt class="img-fluid" />
+											</a>
+										</span>
+										<span class="thumb-info-caption custom-box-shadow">
+											<span class="thumb-info-caption-text">
+												<h4 class="font-weight-bold mb-4">
+													<a href="demo-church-blog-detail.html" class="text-decoration-none custom-secondary-font text-color-dark">
+														Etiam id ex eu velit vestibulum congue
+													</a>
+												</h4>
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at arcu sit amet.</p>
+											</span>
+											<span class="custom-thumb-info-post-infos text-center">
+												<ul>
+													<li class="text-uppercase">
+														<i class="icon-calendar icons"></i>
+														Oct 25
+													</li>
+													<li class="text-uppercase">
+														<i class="icon-user icons"></i>
+														John Doe
+													</li>
+												</ul>
+											</span>
+										</span>
+									</span>
+								</article>
+							</div>
+							<div class="col-lg-4 mb-4 mb-lg-0">
+								<article class="custom-post-blog">
+									<span class="thumb-info custom-thumb-info-2">
+										<span class="thumb-info-wrapper">
+											<a href="demo-church-blog-detail.html">
+												<img src="img/demos/church/blog/blog-4.jpg" alt class="img-fluid" />
+											</a>
+										</span>
+										<span class="thumb-info-caption custom-box-shadow">
+											<span class="thumb-info-caption-text">
+												<h4 class="font-weight-bold mb-4">
+													<a href="demo-church-blog-detail.html" class="text-decoration-none custom-secondary-font text-color-dark">
+														Vestibulum tristique ipsum sed neque
+													</a>
+												</h4>
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at arcu sit amet.</p>
+											</span>
+											<span class="custom-thumb-info-post-infos text-center">
+												<ul>
+													<li class="text-uppercase">
+														<i class="icon-calendar icons"></i>
+														Oct 18
+													</li>
+													<li class="text-uppercase">
+														<i class="icon-user icons"></i>
+														John Doe
+													</li>
+												</ul>
+											</span>
+										</span>
+									</span>
+								</article>
+							</div>
+							<div class="col-lg-4 mb-4 mb-lg-0">
+								<article class="custom-post-blog">
+									<span class="thumb-info custom-thumb-info-2">
+										<span class="thumb-info-wrapper">
+											<a href="demo-church-blog-detail.html">
+												<img src="img/demos/church/blog/blog-5.jpg" alt class="img-fluid" />
+											</a>
+										</span>
+										<span class="thumb-info-caption custom-box-shadow">
+											<span class="thumb-info-caption-text">
+												<h4 class="font-weight-bold mb-4">
+													<a href="demo-church-blog-detail.html" class="text-decoration-none custom-secondary-font text-color-dark">
+														Mauris egestas dui ut dolor bibendum
+													</a>
+												</h4>
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at arcu sit amet.</p>
+											</span>
+											<span class="custom-thumb-info-post-infos text-center">
+												<ul>
+													<li class="text-uppercase">
+														<i class="icon-calendar icons"></i>
+														Oct 20
+													</li>
+													<li class="text-uppercase">
+														<i class="icon-user icons"></i>
+														John Doe
+													</li>
+												</ul>
+											</span>
+										</span>
+									</span>
+								</article>
+							</div>
+							<div class="col-lg-4">
+								<article class="custom-post-blog">
+									<span class="thumb-info custom-thumb-info-2">
+										<span class="thumb-info-wrapper">
+											<a href="demo-church-blog-detail.html">
+												<img src="img/demos/church/blog/blog-6.jpg" alt class="img-fluid" />
+											</a>
+										</span>
+										<span class="thumb-info-caption custom-box-shadow">
+											<span class="thumb-info-caption-text">
+												<h4 class="font-weight-bold mb-4">
+													<a href="demo-church-blog-detail.html" class="text-decoration-none custom-secondary-font text-color-dark">
+														Etiam id ex eu velit vestibulum congue
+													</a>
+												</h4>
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at arcu sit amet.</p>
+											</span>
+											<span class="custom-thumb-info-post-infos text-center">
+												<ul>
+													<li class="text-uppercase">
+														<i class="icon-calendar icons"></i>
+														Oct 25
+													</li>
+													<li class="text-uppercase">
+														<i class="icon-user icons"></i>
+														John Doe
+													</li>
+												</ul>
+											</span>
+										</span>
+									</span>
+								</article>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="section section-tertiary section-no-border m-0">
+					<div class="container">
+						<div class="row align-items-center">
+							<div class="col-lg-10">
+								<span class="custom-secondary-font font-weight-bold custom-text-color-1 text-4">Première visite</span>
+								<h2 class="font-weight-bold custom-text-color-1 m-0">En savoir plus sur l'Église. <span class="font-weight-normal custom-secondary-font custom-font-italic">Votre place est ici</span></h2>
+							</div>
+							<div class="col-lg-2 mt-4 mt-lg-0">
+								<a href="aproposdenous.html" class="btn btn-primary custom-btn-style-1 text-uppercase">Guide des visiteurs</a>
+							</div>
+						</div>
+					</div>
+				</section>
+
+
+				<footer>
+					<?php include('footer.php');?>
+			 
+				</footer>
+			</div>
+		</div>
+
+		<!-- Vendor -->
+		<script src="vendor/plugins/js/plugins.min.js"></script>
+		<script src="vendor/jquery.countdown/jquery.countdown.min.js"></script>
+
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+
+		<!-- Current Page Vendor and Views -->
+		<script src="js/views/view.contact.js"></script>
+
+		<!-- Demo -->
+		<script src="js/demos/demo-church.js"></script>
+
+		<!-- Theme Custom -->
+		<script src="js/custom.js"></script>
+
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+	<!-- Google Maps -->
+		<script>
+
+			/* 
+			Map Markers:
+			- Get an API Key: https://developers.google.com/maps/documentation/javascript/get-api-key
+			- Generate Map Id: https://console.cloud.google.com/google/maps-apis/studio/maps
+			- Use https://www.latlong.net/convert-address-to-lat-long.html to get Latitude and Longitude of a specific address
+			*/
+			(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+				({key: "YOUR_API_KEY", v: "weekly"});
+
+			async function initMap() {
+				const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+				const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+					"marker",
+				);
+				const map = new Map(document.getElementById("googlemaps"), {
+					zoom: 14,
+					center: { lat: -37.817240, lng: 144.955820 },
+					mapId: "YOUR_MAP_API_ID",
+				});
+				const markers = [
+					{
+						position: { lat: -37.817240, lng: 144.955820 },
+						title: "Office 1<br>Melbourne, 121 King St, Australia<br>Phone: 123-456-1234",
+					}
+				];
+
+				const infoWindow = new InfoWindow();
+
+				markers.forEach(({ position, title }, i) => {
+					const pin = new PinElement({
+						background: "#e36159",
+						borderColor: "#e36159",
+						glyphColor: "#FFF",
+					});
+					const marker = new AdvancedMarkerElement({
+						position,
+						map,
+						title: `${title}`,
+						content: pin.element,
+					});
+
+					marker.addListener("click", ({ domEvent, latLng }) => {
+						const { target } = domEvent;
+
+						infoWindow.close();
+						infoWindow.setContent(marker.title);
+						infoWindow.open(marker.map, marker);
+					});
+				});	
+
+			}
+
+			initMap();	
+
+		</script>
+
+	</body>
+</html>
