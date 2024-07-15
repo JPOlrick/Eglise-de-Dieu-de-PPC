60bf8f8bab1b46df627364c99056245dfcf94258
Olrick Jean-Pierre <olrick.jeanpierre02@gmail.com>
2024-07-15 10:02:18 -0400

    Update header.php

--- header.php
+++ header.php
@@ -52,142 +52,141 @@
 	<body>
 
 	<div class="body">
-	<header id="header" class="header-effect-shrink" data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': false, 'stickyChangeLogo': false, 'stickyStartAt': 100, 'stickyHeaderContainerHeight': 70}">
-		<div class="header-body border-top-0">
-			<div class="header-top">
-				<div class="container-fluid"> <!-- Changement ici -->
-					<div class="row justify-content-between">
-						<div class="col-auto">
-							<p class="alternative-font-4 mb-0"><strong class="text-color-dark">Église de Dieu de Petite Place Cazeau</strong></p>
-						</div>
-						<div class="col-auto d-flex align-items-center">
-							<nav>
-								<ul class="list list-unstyled d-flex text-1 mb-0">
-									<li class="mb-0"><a href="aproposdenous.php#talkus" class="alternative-font-4 text-decoration-none text-3-5 font-weight-bold">PLANIFIEZ VOTRE VISITE</a></li>
-									<li class="mb-0 ms-4-5 "><a href="evenements.php" class="alternative-font-4 text-decoration-none text-3-5 font-weight-bold">CALENDRIER</a></li>
-									<li class="mb-0 ms-4-5 "><a href="donation.php" class="alternative-font-4 text-decoration-none text-3-5 font-weight-bold">FAIRE UN DON</a></li>
-								</ul>
-							</nav>
-							<div class="header-nav-features d-none d-md-block ms-5-5">
-								<ul class="header-social-icons social-icons social-icons-clean social-icons-icon-dark social-icons-medium me-2 mb-0 d-none d-sm-block ms-1">
-									<li class="social-icons-instagram">
-										<a href="http://www.instagram.com/eglisededieuppc" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a>
-									</li>
-									<li class="social-icons-youtube">
-										<a href="http://www.youtube.com/@eglisededieudepetite-place3217" target="_blank" title="YouTube"><i class="fab fa-youtube"></i></a>
-									</li>
-									<li class="social-icons-facebook">
-										<a href="http://www.facebook.com/eglisededieuppc" target="_blank" title="Facebook"><i class="fab fa-facebook-f"></i></a>
-									</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="header-container container-fluid"> <!-- Changement ici -->
-				<div class="header-row">
-					<div class="header-column">
-						<div class="header-row">
-							<div class="header-logo">
-								<a href="index.php">
-									<img alt="EDPPC" width="200" height="100" src="img/EDPPC.png">
-								</a>
-							</div>
-						</div>
-					</div>
-					<div class="header-column justify-content-end">
-						<div class="header-row">
-							<div class="header-nav order-2 order-lg-1 alternative-font-4">
-								<div class="header-nav-main header-nav-main-square header-nav-main-dark-text header-nav-main-effect-1 header-nav-main-sub-effect-1">
-									<nav class="collapse">
-										<ul class="nav nav-pills" id="mainNav">
-											<li>
-												<a class=" nav-link <?php echo $current_page == 'index.php' ? 'active' : ''; ?>" href="index.php">
-													Accueil
-												</a>
-											</li>
-											<li class="dropdown dropdown-full-color dropdown-primary">
-												<a class="dropdown-item dropdown-toggle <?php echo $current_page == 'aproposdenous.php' ? 'active' : ''; ?>" href="aproposdenous.php">
-													À propos de nous
-												</a>
-												<ul class="dropdown-menu">
-													<li>
-														<a class="dropdown-item <?php echo $current_page == 'apropos.php' ? 'active' : ''; ?>" href="apropos.php">Vue d'ensemble</a>
-													</li>
-													<li>
-														<a class="dropdown-item" href="demo-church-about-us-staff.html">Le personnel</a>
-													</li>
-													<li>
-														<a class="dropdown-item" href="demo-church-about-us-locations.html">Localisations</a>
-													</li>
-												</ul>
-											</li>
-											<li>
-												<a class="nav-link <?php echo $current_page == 'sermons.php' ? 'active' : ''; ?>" href="demo-church-sermons.html">
-													Sermons
-												</a>
-											</li>
-											<li>
-												<a class="nav-link <?php echo $current_page == 'evenements.php' ? 'active' : ''; ?>" href="evenements.php">
-													Événements
-												</a>
-											</li>
-											<li>
-												<a class="nav-link <?php echo $current_page == 'galerie.php' ? 'active' : ''; ?>" href="galerie.php">
-													Galerie
-												</a>
-											</li>
-											<li class="dropdown dropdown-full-color dropdown-primary">
-												<a class="dropdown-item dropdown-toggle <?php echo $current_page == 'blog.php' ? 'active' : ''; ?>" href="blog.php">
-													Blog
-												</a>
-												<ul class="dropdown-menu">
-													<li>
-														<a class="dropdown-item <?php echo $current_page == 'blog.php' ? 'active' : ''; ?>" href="aproposdenous.php">Les départements</a>
-													</li>
-													<li>
-														<a class="dropdown-item <?php echo $current_page == 'blog.php' ? 'active' : ''; ?>" href="demo-church-about-us-staff.html">Les groupes</a>
-													</li>
-												</ul>
-											</li>
-											<li>
-												<a class="nav-link <?php echo $current_page == 'contacteznous.php' ? 'active' : ''; ?>" href="contacteznous.php">
-													Contactez-nous
-												</a>
-											</li>
+			<header id="header" class="header-effect-shrink" data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': false, 'stickyChangeLogo': false, 'stickyStartAt': 100, 'stickyHeaderContainerHeight': 70}">
+				<div class="header-body border-top-0">
+					<div class="header-top">
+						<div class="container">
+							<div class="row justify-content-between">
+								<div class="col-auto">
+									<p class="alternative-font-4 mb-0"><strong class="text-color-dark">Église de Dieu de Petite Place Cazeau</strong></p>
+								</div>
+								<div class="col-auto d-flex align-items-center">
+									<nav>
+										<ul class="list list-unstyled d-flex text-1 mb-0">
+											<li class="mb-0"><a href="aproposdenous.php#talkus" class="alternative-font-4 text-decoration-none text-3-5 font-weight-bold">PLANIFIEZ VOTRE VISITE</a></li>
+											<li class="mb-0 ms-4-5 "><a href="evenements.php" class="alternative-font-4 text-decoration-none text-3-5 font-weight-bold">CALENDRIER</a></li>
+											<li class="mb-0 ms-4-5 "><a href="donation.php" class="alternative-font-4 text-decoration-none text-3-5 font-weight-bold">FAIRE UN DON</a></li>
 										</ul>
 									</nav>
+									<div class="header-nav-features d-none d-md-block ms-5-5">
+										<ul class="header-social-icons social-icons social-icons-clean social-icons-icon-dark social-icons-medium me-2 mb-0 d-none d-sm-block ms-1">
+											<li class="social-icons-instagram">
+												<a href="http://www.instagram.com/eglisededieuppc" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a>
+											</li>
+											<li class="social-icons-youtube">
+												<a href="http://www.youtube.com/@eglisededieudepetite-place3217" target="_blank" title="YouTube"><i class="fab fa-youtube"></i></a>
+											</li>
+											<li class="social-icons-facebook">
+												<a href="http://www.facebook.com/eglisededieuppc" target="_blank" title="Facebook"><i class="fab fa-facebook-f"></i></a>
+											</li>
+										</ul>
+									</div>
 								</div>
-								<button class="btn header-btn-collapse-nav" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav">
-									<i class="fas fa-bars"></i>
-								</button>
+							</div>
+						</div>
+					</div>
+					<div class="header-container container">
+						<div class="header-row ">
+							<div class="header-column">
+								<div class="header-row">
+									<div class="header-logo">
+										<a href="index.php">
+											<img alt="EDPPC" width="200" height="100" src="img/EDPPC.png">
+										</a>
+									</div>
+								</div>
+							</div>
+							<div class="header-column justify-content-end">
+								<div class="header-row">
+									<div class="header-nav order-2 order-lg-1 alternative-font-4">
+										<div class="header-nav-main header-nav-main-square header-nav-main-dark-text header-nav-main-effect-1 header-nav-main-sub-effect-1">
+											<nav class="collapse">
+												<ul class="nav nav-pills" id="mainNav">
+													<li>
+														<a class=" nav-link <?php echo $current_page == 'index.php' ? 'active' : ''; ?>" href="index.php">
+															Accueil
+														</a>
+													</li>
+													<li class="dropdown dropdown-full-color dropdown-primary">
+														<a class="dropdown-item dropdown-toggle <?php echo $current_page == 'aproposdenous.php' ? 'active' : ''; ?>" href="aproposdenous.php">
+															À propos de nous
+														</a>
+														<ul class="dropdown-menu">
+															<li>
+																<a class="dropdown-item <?php echo $current_page == 'apropos.php' ? 'active' : ''; ?>" href="apropos.php">Vue d'ensemble</a>
+															</li>
+															<li>
+																<a class="dropdown-item" href="demo-church-about-us-staff.html">Le personnel</a>
+															</li>
+															<li>
+																<a class="dropdown-item" href="demo-church-about-us-locations.html">Localisations</a>
+															</li>
+														</ul>
+													</li>
+													<li>
+														<a class="nav-link <?php echo $current_page == 'sermons.php' ? 'active' : ''; ?>" href="demo-church-sermons.html">
+															Sermons
+														</a>
+													</li>
+													<li>
+														<a class="nav-link <?php echo $current_page == 'evenements.php' ? 'active' : ''; ?>" href="evenements.php">
+															Événements
+														</a>
+													</li>
+													<li>
+														<a class="nav-link <?php echo $current_page == 'galerie.php' ? 'active' : ''; ?>" href="galerie.php">
+															Galerie
+														</a>
+													</li>		
+													<li class="dropdown dropdown-full-color dropdown-primary">
+															<a class="dropdown-item dropdown-toggle <?php echo $current_page == 'blog.php' ? 'active' : ''; ?>" href="blog.php">
+																Blog
+															</a>
+															<ul class="dropdown-menu">
+																<li>
+																	<a class="dropdown-item <?php echo $current_page == 'blog.php' ? 'active' : ''; ?>" href="aproposdenous.php">Les départements</a>
+																</li>
+																<li>
+																	<a class="dropdown-item <?php echo $current_page == 'blog.php' ? 'active' : ''; ?>" href="demo-church-about-us-staff.html">Les groupes</a>
+																</li>
+															</ul>
+													</li>
+													<li>
+														<a class="nav-link <?php echo $current_page == 'contacteznous.php' ? 'active' : ''; ?>" href="contacteznous.php">
+															Contactez-nous
+														</a>
+													</li>
+												</ul>
+											</nav>
+										</div>
+										<button class="btn header-btn-collapse-nav" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav">
+											<i class="fas fa-bars"></i>
+										</button>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-	</header>
-</div>
+			</header>
 
-<!-- Vendor -->
-<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
-<script src="vendor/plugins/js/plugins.min.js"></script>
-<script src="vendor/jquery.countdown/jquery.countdown.min.js"></script>
+			  
 
-<!-- Theme Base, Components and Settings -->
-<script src="js/theme.js"></script>
 
-<!-- Current Page Vendor and Views -->
-<script src="js/views/view.contact.js"></script>
+		<!-- Vendor -->		<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="vendor/plugins/js/plugins.min.js"></script>		<script src="vendor/jquery.countdown/jquery.countdown.min.js"></script>
 
-<!-- Demo -->
-<script src="js/demos/demo-church.js"></script>
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
 
-<!-- Theme Initialization Files -->
-<script src="js/theme.init.js"></script>
+		<!-- Current Page Vendor and Views -->
+		<script src="js/views/view.contact.js"></script>
 
+		<!-- Demo -->
+		<script src="js/demos/demo-church.js"></script>
+
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
 
 	</body>
+	
 </html>
